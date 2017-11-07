1f34646ba260636db5d6af6fa5e5d283fd9503f1
bisthebis <bism25@hotmail.com>
2017-11-07 21:03:46 +0100

    Fixed typo

--- json_lib/json_types.cpp
+++ json_lib/json_types.cpp
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-#include "json_types.h".h"
+#include "json_types.h"
 #include <boost/variant.hpp>
 #include <stdexcept>
 
